05a8a757ee78ad87c20fc059b040b8956e80e759
Arhqirah <Simonmelbyeandersen@hotmail.com>
2022-04-22 10:07:12 +0200

    Udskrift

--- src/com/company/BestiltPizza.java
+++ src/com/company/BestiltPizza.java
@@ -1,6 +1,15 @@
 package com.company;
 
+
+
 public class BestiltPizza extends Pizza {
+
+    public String red = "\u001B[31m";
+    public String fReset = "\u001B[0m";
+    public String green = "\u001B[32m";
+    public String blue = "\u001B[34m";
+    public String yellow = "\u001B[33m";
+    public String cyan = "\u001B[36m";
 
     private String tidspunkt;
     private int kundeTlf;
@@ -26,8 +35,37 @@ public class BestiltPizza extends Pizza {
         return takeaway;
     }
 
-    @Override
+    public String toString() {
+        StringBuilder text = new StringBuilder();
+        text
+
+                .append("\t")
+                .append(red +" Tlf: " + fReset)
+                .append(getKundeTlf())
+                .append("\t")
+                .append(" Tidspunkt:")
+                .append(getTidspunkt())
+                .append("\t")
+                .append(" Takeaway:")
+                .append(getTakeaway())
+                .append("\t")
+                .append("#")
+                .append(getPizzaNum())
+                .append("\t")
+                .append(getPizzaPris())
+                .append(" Kr")
+                .append("\t ")
+                .append(getPizzaName())
+                .append("\t")
+                .append(new UI().getColorString("cyan", getPizzaBeskrivelse()));
+
+        return text.toString();
+    }
+}
+  /*  @Override
     public String toString() {
         return "Nr " + getPizzaNum() + ". " + getPizzaName() + " " + getPizzaBeskrivelse() + " - " + getPizzaPris() + "kr.- " + " Telefon: " + kundeTlf + " " + "Tidspunkt: " + tidspunkt + " Takeaway: " + takeaway;
     }
 }
+
+   */
